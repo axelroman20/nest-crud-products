c3b0a3f5b440dda27ed1329b1d1059e3efae379d
Axel Roman <axelroman20@gmail.com>
2023-05-15 14:02:36 -0600

    resource seed added

--- README.md
+++ README.md
@@ -18,7 +18,12 @@ yarn install
 docker-compose up -d
 ```
 
-6. Levantar el proyecto
+6. Ejecutar el SEED
+```
+http://localhost:3000/api/seed
+```
+
+7. Levantar el proyecto
 ```
 yarn start:dev
 ```
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -8,5 +8,6 @@ import { ProductsService } from './products.service';
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
