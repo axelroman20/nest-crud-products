42ee78a721d7fa5f7d9c216ed24988440c2d03f2
Axel Roman <axelroman20@gmail.com>
2023-05-16 15:20:36 -0600

    encrypt password user seed added

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,6 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-import { Auth, GetUser } from '../auth/decorators';
-import { User } from '../auth/entities/user.entity';
 import { SeedService } from './seed.service';
 
 @Controller('seed')
@@ -8,8 +6,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth()
-  executeSeed(@GetUser() user: User) {
-    return this.seedService.runSeed(user);
+  executeSeed() {
+    return this.seedService.runSeed();
   }
 }
