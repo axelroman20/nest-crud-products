ec9a5e49503f62fbd0ebcfdb4149f4c1e1bea23f
Axel Roman <axelroman20@gmail.com>
2023-05-17 13:57:12 -0600

    config prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,20 @@ import { SeedModule } from './seed/seed.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      autoLoadEntities: true, //fix: solo em modo dev
-      synchronize: true,
+      //fix: solo em modo dev
+      autoLoadEntities: process.env.STAGE === 'dev',
+      synchronize: process.env.STAGE === 'dev',
     }),
     ProductsModule,
     SeedModule,
